2af33ec3282f246cf37637c91ed54346a55eff0a
adampush <ajonpush@gmail.com>
2016-10-02 17:59:01 -0400

    Refactor code to load word lists and add TODOs
    Refactor the code that loads the word lists to be cleaner and more
    readable. Split up some long lines into multiple statements for
    readability and clarity. Add TODO items.

--- pypw/pypw.py
+++ pypw/pypw.py
@@ -27,28 +27,26 @@ __version__ = '0.1'
 
 # This code should get executed on import. This will read in the tab-delimited wordlist file and convert it into a
 # dictionary keyed on numerical codes so that we can easily look up words that match our rolls.
+# TODO: we are doing the same thing three times here, could consolidate. Perhaps turn into function so can unit-test
 sep = os.sep
-list_of_lists = list(csv.reader(open(os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_1.txt',
-                                     'r'), delimiter='\t'))
-WORD_LIST_EFF_SMALL_1 = dict()
-for listItem in list_of_lists:
-    WORD_LIST_EFF_SMALL_1[listItem[0]] = listItem[1]
+file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_1.txt'
+with open(file_path, 'r') as file:
+    list_of_lists = list(csv.reader(file, delimiter='\t'))
+    WORD_LIST_EFF_SMALL_1 = {list_item[0]: list_item[1] for list_item in list_of_lists}
 
-list_of_lists = list(csv.reader(open(os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_2.txt',
-                                     'r'), delimiter='\t'))
-WORD_LIST_EFF_SMALL_2 = dict()
-for listItem in list_of_lists:
-    WORD_LIST_EFF_SMALL_2[listItem[0]] = listItem[1]
+file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_2.txt'
+with open(file_path, 'r') as file:
+    list_of_lists = list(csv.reader(file, delimiter='\t'))
+    WORD_LIST_EFF_SMALL_2 = {listItem[0]: listItem[1] for listItem in list_of_lists}
 
-list_of_lists = list(csv.reader(open(os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_large_wordlist.txt',
-                                     'r'), delimiter='\t'))
-WORD_LIST_EFF_LARGE = dict()
-for listItem in list_of_lists:
-    WORD_LIST_EFF_LARGE[listItem[0]] = listItem[1]
+file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_large_wordlist.txt'
+with open(file_path, 'r') as file:
+    list_of_lists = list(csv.reader(file, delimiter='\t'))
+    WORD_LIST_EFF_LARGE = {listItem[0]: listItem[1] for listItem in list_of_lists}
 
+# TODO: I think these constants should be relocated to just after the import statements
 DIE_STRING_EFF_SMALL = '4d6'  # Roll 4d6 per roll if the wordlist is one of the two EFF "small" lists
 DIE_STRING_EFF_LARGE = '5d6'  # Roll 5d6 per roll if the wordlist is the large EFF list (not implemented)
-
 # For some reason the source txt file for the EFF small wordlist #1 has only four digits that vary but it has one
 # constant digit in the MSB position that is constant=1.
 EFF_SMALL_1_PREFIX_HACK = '1'
@@ -60,7 +58,7 @@ def generate(num_words=DEFAULT_NUM_WORDS, word_list_to_use=WORD_LIST_EFF_SMALL_1
     """Generate a diceware password. If called with no arguments, generates a
     six-word password using the EFF "Small Wordlist #1."
 
-    :type num_words: int
+    :type num_words: integer
     :param word_list_to_use:
     :param num_words:
     :return:
