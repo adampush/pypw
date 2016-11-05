9c2ce343e5fc3ee9b4b91d5f1027ef568a2cd157
adampush <ajonpush@gmail.com>
2016-11-05 13:30:24 -0400

    Refactor wordlist import and make other clarifications.
    Refactor wordlist import code to use a new function,
    "_import_word_lists". Function is called once for each word list.
    Remove unused imports (__future__, pyparsing ParseException).
    Remove assignment to __all__. Move "caps constant" definitions
    to just after import statments and __author__ and __version__
    assignments. Change default word list to "EFF Large." Fix
    bug with die string selection logic. Refactor code list creation
    logic for clarity. Add/update comments. Remove function "test".

--- pypw/pypw.py
+++ pypw/pypw.py
@@ -13,50 +13,45 @@ Example of basic usage:
     13165 -> fruit
     GraspGivenUnitLakeSkidFruit
 """
-from __future__ import absolute_import, print_function, unicode_literals
-
-from pyparsing import ParseException
 
 import pypw.dice as dice
 import csv
 import os
 
-__all__ = ['roll', 'ParseException']
 __author__ = "Adam Push"
 __version__ = '0.1'
 
-# This code should get executed on import. This will read in the tab-delimited wordlist file and convert it into a
-# dictionary keyed on numerical codes so that we can easily look up words that match our rolls.
-# TODO: we are doing the same thing three times here, could consolidate. Perhaps turn into function so can unit-test
-sep = os.sep
-file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_1.txt'
-with open(file_path, 'r') as file:
-    list_of_lists = list(csv.reader(file, delimiter='\t'))
-    WORD_LIST_EFF_SMALL_1 = {list_item[0]: list_item[1] for list_item in list_of_lists}
-
-file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_2.txt'
-with open(file_path, 'r') as file:
-    list_of_lists = list(csv.reader(file, delimiter='\t'))
-    WORD_LIST_EFF_SMALL_2 = {listItem[0]: listItem[1] for listItem in list_of_lists}
-
-file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_large_wordlist.txt'
-with open(file_path, 'r') as file:
-    list_of_lists = list(csv.reader(file, delimiter='\t'))
-    WORD_LIST_EFF_LARGE = {listItem[0]: listItem[1] for listItem in list_of_lists}
-
-# TODO: I think these constants should be relocated to just after the import statements
 DIE_STRING_EFF_SMALL = '4d6'  # Roll 4d6 per roll if the wordlist is one of the two EFF "small" lists
-DIE_STRING_EFF_LARGE = '5d6'  # Roll 5d6 per roll if the wordlist is the large EFF list (not implemented)
+DIE_STRING_EFF_LARGE = '5d6'  # Roll 5d6 per roll if the wordlist is the large EFF list
 # For some reason the source txt file for the EFF small wordlist #1 has only four digits that vary but it has one
 # constant digit in the MSB position that is constant=1.
 EFF_SMALL_1_PREFIX_HACK = '1'
 
-DEFAULT_NUM_WORDS = 6  # Default number of words is 6... avoid magic numbers
+DEFAULT_NUM_WORDS = 6
 
 
-def generate(num_words=DEFAULT_NUM_WORDS, word_list_to_use=WORD_LIST_EFF_SMALL_1):
+# This code should get executed on import. This will read in the tab-delimited wordlist file and convert it into a
+# dictionary keyed on numerical codes so that we can easily look up words that match our rolls.
+def _import_word_lists(file_path):
+    with open(file_path, 'r') as file:
+        list_of_lists = list(csv.reader(file, delimiter='\t'))
+        return {list_item[0]: list_item[1] for list_item in list_of_lists}
+
+# Create word list dictionaries
+sep = os.sep
+file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_1.txt'
+WORD_LIST_EFF_SMALL_1 = _import_word_lists(file_path)
+
+file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_2.txt'
+WORD_LIST_EFF_SMALL_2 = _import_word_lists(file_path)
+
+file_path = os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_large_wordlist.txt'
+WORD_LIST_EFF_LARGE = _import_word_lists(file_path)
+
+
+def generate(num_words=DEFAULT_NUM_WORDS, word_list_to_use=WORD_LIST_EFF_LARGE):
     """Generate a diceware password. If called with no arguments, generates a
-    six-word password using the EFF "Small Wordlist #1."
+    six-word password using the EFF "Large Word List."
 
     :type num_words: integer
     :param word_list_to_use:
@@ -64,17 +59,28 @@ def generate(num_words=DEFAULT_NUM_WORDS, word_list_to_use=WORD_LIST_EFF_SMALL_1
     :return:
     """
 
-    if word_list_to_use == WORD_LIST_EFF_SMALL_1 or WORD_LIST_EFF_SMALL_2:
+    # Decide which word list to use based on function argument
+    if word_list_to_use is WORD_LIST_EFF_SMALL_1:
         die_string_to_use = DIE_STRING_EFF_SMALL
-    elif word_list_to_use == WORD_LIST_EFF_LARGE:
+    elif word_list_to_use is WORD_LIST_EFF_SMALL_2:
+        die_string_to_use = DIE_STRING_EFF_SMALL
+    elif word_list_to_use is WORD_LIST_EFF_LARGE:
         die_string_to_use = DIE_STRING_EFF_LARGE
     else:
         die_string_to_use = DIE_STRING_EFF_SMALL
 
-    # TODO: this is ugly but works, maybe split into two separate expressions for readability
-    code_list = [''.join([str(die) for die in roll(die_string_to_use)]) for i in range(0, num_words)]
+    # Create code list from die rolls
+    code_list = []
+    for i in range(num_words):
+        code_list.append(''.join([str(die) for die in roll(die_string_to_use)]))
 
-    if word_list_to_use == WORD_LIST_EFF_SMALL_1:
+    # Older, less readable way to create code list from die rolls
+    # code_list = [''.join([str(die) for die in roll(die_string_to_use)]) for i in range(0, num_words)]
+
+    # Use the code list to look up words in the word list and add them to the password list. If the word list is
+    # "EFF Small #1", apply the "prefix hack" to prepend each word code with a prefix (this is due to the word
+    # list file being a little wonky). TODO: should fix the source wordlist file so we do not have to do this.
+    if word_list_to_use is WORD_LIST_EFF_SMALL_1:
         password_word_list = [word_list_to_use[EFF_SMALL_1_PREFIX_HACK + code_string] for code_string in code_list]
     else:
         password_word_list = [word_list_to_use[code_string] for code_string in code_list]
@@ -94,15 +100,6 @@ def roll(string, single=True, verbose=False):
     :return:
     """
 
+    # TODO: using the dice library is way overkill. Rewrite to just use "random" module
+
     return dice.roll(string, single, verbose)
-
-
-def test():
-    """This is just a test function for playing with things."""
-
-    print(os.path.dirname(__file__))
-
-    sep = os.sep
-    lol = list(csv.reader(open(os.path.dirname(__file__) + sep + 'wordlists' + sep + 'eff_small_wordlist_1.txt', 'r'),
-                          delimiter='\t'))
-    print(lol)
